0ae692ab9e68bf16a8e6b306c18e832eaf09894d
MuhammadubaidAli <162048631+MuhammadubaidAli@users.noreply.github.com>
2024-05-13 08:44:41 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+#!usr/bin/env/node
+import inquirer from "inquirer";
+const randomNumber=Math.floor(Math.random()*7+1);
+
+const answers = await inquirer.prompt([
+    {
+        name:"userguessnumber",
+        type:"number",
+        message:"Please guess a number 1 _to_ 6?",
+    }
+]);
+console.log(answers);
+if (answers.userguessnumber===randomNumber){
+    console.log("Congratulation`s ,you select right number");
+}else{
+    console.log ("Eat almond")
+}
